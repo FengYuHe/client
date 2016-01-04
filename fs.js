0054f7eb109d58c2b61038deefbe3afffcb04556
fengyuhe <heyufeng8080@gmail.com>
2016-01-04 17:30:42 +0800

    update package

--- fs.js
+++ fs.js
@@ -5,7 +5,7 @@ var readFile =function(){
 };
 
 readFile.getToken = function(){
-	var node = fs.readFileSync('../public/fs/nodeToken.txt','utf-8');
+	var node = fs.readFileSync('public/fs/nodeToken.txt','utf-8');
 	node = JSON.parse(node);
 	return node.data.token;
 };
